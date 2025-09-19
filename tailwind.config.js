8bd1d5ec44bccb9c5bbe8bdf7648d5ea0da655bc
Naoya Fukada <naoyafukada@Naoyas-MacBook-Pro.local>
2025-09-19 10:00:02 +1000

    Imprement basic of nav

---
+++ tailwind.config.js
@@ -0,0 +1,15 @@
+// module.exports = {
+//   theme: {
+//     extend: {
+//       colors: {
+//         bg: "#0C2D48",
+//         charcoal: "#111827",
+//         mint: "#5FE3C0",
+//         text: "#F3F4F6",
+//         muted: "#9CA3AF",
+//         cta: "#14B8A6",
+//         line: "#2DD4BF",
+//       },
+//     },
+//   },
+// };
