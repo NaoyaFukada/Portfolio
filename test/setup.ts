a128f1e2eb9b74e745d7a25feb7afcb737f3de04
Naoya Fukada <naoyafukada@Naoyas-MacBook-Pro.local>
2025-09-23 00:38:58 +1000

    Fix Attempt

--- test/setup.ts
+++ test/setup.ts
@@ -1,2 +1,8 @@
 // Reference: https://github.com/testing-library/jest-dom?tab=readme-ov-file#with-vitest
 import "@testing-library/jest-dom/vitest";
+
+if (typeof TextEncoder === "undefined") {
+  const { TextEncoder, TextDecoder } = require("util");
+  global.TextEncoder = TextEncoder;
+  global.TextDecoder = TextDecoder;
+}
