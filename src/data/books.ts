13b9a18c668e95ef33e0f53e8c4e9760cabeb217
Naoya Fukada <naoyafukada@Naoyas-MacBook-Pro.local>
2025-10-05 15:44:10 +1100

    Education Section

---
+++ src/data/books.ts
@@ -0,0 +1,38 @@
+interface Book {
+  title: string;
+  author: string;
+  category: string;
+}
+
+export const favoriteBooks: Book[] = [
+  {
+    title: "Clean Code",
+    author: "Robert C. Martin",
+    category: "Software Craftsmanship",
+  },
+  {
+    title: "Design Patterns",
+    author: "Gang of Four",
+    category: "Software Architecture",
+  },
+  {
+    title: "The Pragmatic Programmer",
+    author: "Andrew Hunt & David Thomas",
+    category: "Software Development",
+  },
+  {
+    title: "You Don't Know JS",
+    author: "Kyle Simpson",
+    category: "JavaScript",
+  },
+  {
+    title: "Introduction to Algorithms",
+    author: "Cormen, Leiserson, Rivest & Stein",
+    category: "Algorithms",
+  },
+  {
+    title: "Eloquent JavaScript",
+    author: "Marijn Haverbeke",
+    category: "JavaScript",
+  },
+];
